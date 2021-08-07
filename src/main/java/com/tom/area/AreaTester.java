b213ebe1932f50714201669d866ed601a97f5252
Hank Tom <hanktom@gmail.com>
2021-08-07 21:44:14 +0800

    interface implementation without a class name

--- src/main/java/com/tom/area/AreaTester.java
+++ src/main/java/com/tom/area/AreaTester.java
@@ -9,6 +9,14 @@ public class AreaTester {
         shapes.add(new Triangle(10, 10));
         shapes.add(new Rectangle(10, 10));
         shapes.add(new Triangle(20, 30));
+        int r = 10;
+        Shape circle = new Shape() {
+            @Override
+            public float getArea() {
+                return 10*10*3.14159f;
+            }
+        };
+        shapes.add(circle);
         for (Shape shape : shapes) {
             System.out.println(shape.getArea());
         }
---
+++ src/main/java/com/tom/db/BoxExam.java
@@ -0,0 +1,20 @@
+package com.tom.db;
+
+//建立資料庫 box
+//建立表格box
+//建立2-3筆郵便箱資料 id, name, length, width, height, price
+public class BoxExam {
+    public static void main(String[] args) {
+        //connect to database(localhost)
+
+
+        //Please enter your object length:
+
+        //Please enter your object width:
+
+        //Please enter your object height:
+
+        //box name, price or no box for you
+
+    }
+}
